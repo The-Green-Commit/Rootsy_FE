18f739826ce6f5b7701a6a1d3b4b8988ddae254b
Priscila <pguillen.oliv@gmail.com>
2025-05-21 09:45:14 +0200

    create basic footer structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import { css } from '@emotion/react'
+
+function Footer() {
+    return (
+        <footer className="fixed size-5 bottom-0 left-0 w-full bg-[#DDE5B6] items-center py-4 shadow-md">
+            <div className="flex justify-center">
+                <img src="src\assets\logos\commit-me.png"/>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
