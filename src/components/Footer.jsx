fb674a1f58fbd3374b08092b79133c16a1c36e2d
Priscila <pguillen.oliv@gmail.com>
2025-05-21 11:44:38 +0200

    add footer and styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,31 @@
-import { css } from '@emotion/react'
+/** @jsxImportSource @emotion/react */
 import theme from '../themes/MainTheme';
+import { css } from '@emotion/react'
 
 function Footer() {
     return (
         <footer 
             css={css`
-                display: flex,
-                justify-content: center,
-                background-color: ${theme.palette.lime.main},
-                left: 0,
-                bottom: 0,
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background-color: ${theme.palette.lime.main};
+                position: fixed;
+                width: -webkit-fill-available;
+                height: 10vh;
+                left: 0;
+                bottom: 0;
                 z-index: 100`}
             >
-            <div
-            css={css`
-                width: auto,
-                length: 20px`}
-            >
-                <img src="src\assets\logos\commit-me.png"/>
-            </div>
+            {/* // css={css`
+            //     width: auto;
+            //     length: 20px`}
+             */}
+                <img src="src/assets/logos/commit-me_logo.png"
+                    css={css`
+                        width: auto;
+                        height: 35px`}
+                />
         </footer>
     )
 }
