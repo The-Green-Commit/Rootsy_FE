680933f90d82d822f3980c9603b6846fac04f050
Priscila <pguillen.oliv@gmail.com>
2025-05-23 01:44:22 +0200

    create basic page structure

---
+++ src/pages/PlantsearchPage.jsx
@@ -0,0 +1,12 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
+function PlantsearchPage() {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default PlantsearchPage;
