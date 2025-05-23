96d897a8409e8ded00197c061f74770f90aa49ad
Priscila <pguillen.oliv@gmail.com>
2025-05-23 09:04:10 +0200

    add css and title

--- src/pages/PlantsearchPage.jsx
+++ src/pages/PlantsearchPage.jsx
@@ -4,7 +4,19 @@ import { css } from '@emotion/react'
 function PlantsearchPage() {
     return (
         <>
-        
+        <main
+            css={css`
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                height: 97vh;
+                font-family: 'Roboto';`}
+        >
+            <h2>
+                Search
+            </h2>
+        </main>
         </>
     )
 }
